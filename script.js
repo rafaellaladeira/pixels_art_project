7afb1ea6a7945056548826340f5f0b02bfdbdb41
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2021-12-09 18:34:23 -0300

    Requisitos 2 e 3

---
+++ script.js
@@ -0,0 +1,11 @@
+/* const colorPalette = document.querySelector('section')[0];
+const colorItem = document.createElement('div');
+
+const array = ['black', 'blue', 'pink', 'purple'];
+for (let i = 0; i < array.length; i += 1) {
+  colorItem.innerText = array[i];
+  colorItem.classList.add('color');
+  colorPalette.appendChild(colorItem);
+}
+
+*/
