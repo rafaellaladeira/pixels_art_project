1a20d58776213837b4dacbe38927d84eb84747d5
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2021-12-14 07:22:00 -0300

    Requisitos 4 e 5

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-/* const colorPalette = document.querySelector('section')[0];
-const colorItem = document.createElement('div');
+const colorPalette = document.querySelector('section');
+const color1 = document.createElement('div');
+const color2 = document.createElement('div');
+const color3 = document.createElement('div');
+const color4 = document.createElement('div');
+color1.className = 'color um';
+color2.className = 'color dois';
+color3.className = 'color tres';
+color4.className = 'color quatro';
+colorPalette.appendChild(color1);
+colorPalette.appendChild(color2);
+colorPalette.appendChild(color3);
+colorPalette.appendChild(color4);
 
-const array = ['black', 'blue', 'pink', 'purple'];
-for (let i = 0; i < array.length; i += 1) {
-  colorItem.innerText = array[i];
-  colorItem.classList.add('color');
-  colorPalette.appendChild(colorItem);
+const bigPixel = document.getElementById('pixel-board');
+
+function makeRows(rows, cols) {
+  bigPixel.style.setProperty('--grid-rows', rows);
+  bigPixel.style.setProperty('--grid-cols', cols);
+  for (let i = 0; i < (rows * cols); i += 1) {
+    const pixel = document.createElement('div');
+    bigPixel.appendChild(pixel).className = 'pixel';
+  }
 }
+makeRows(5, 5);
 
-*/
+// Referência para a questão 4: https://stackoverflow.com/questions/57550082/creating-a-16x16-grid-using-javascript
+
